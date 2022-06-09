c9665573d292369a29a92c165ddccbeb2d3f9561
uolter <walter.traspadini@pagopa.it>
2022-06-09 15:02:01 +0200

    load test with plow

--- plow/01-patch-user.sh
+++ plow/01-patch-user.sh
@@ -2,14 +2,14 @@
 
 HOST='api.pdv.pagopa.it'
 API_KEY=${1}
-SLEEP=$(( 120 * 60 ))
+SLEEP=$(( 1 * 1 ))
 
 
-for i in {1..5}
+for i in {1..1}
 do
   startat=$(date)
   echo "Rount ${i} [${startat}]"
-  plow https://${HOST}/user-registry/v1/users -c 1 -n 100 -d 2m \
+  plow https://${HOST}/user-registry/v1/users -c 1 -d 12m \
     --body @file.json \
     --header=x-api-key:${API_KEY} \
     -T 'application/json' -m PATCH 
