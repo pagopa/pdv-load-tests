8cec9a8cd0a2ec36f163eab6aa958308af93804c
uolter <walter.traspadini@pagopa.it>
2022-06-14 18:40:22 +0200

    echo aws session token for troubleshooting

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 # Start cloudwatch agent
+
+echo "AWS SESSION TOKEN $AWS_SESSION_TOKEN"
+
 /opt/aws/amazon-cloudwatch-agent/bin/start-amazon-cloudwatch-agent&
 sleep 10
 K6_STATSD_ENABLE_TAGS=true k6 run --out statsd $1
