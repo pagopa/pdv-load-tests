0678c84d03d211b79539e356d2ff803627e1df9a
uolter <walter.traspadini@pagopa.it>
2022-06-16 11:51:58 +0200

    write credential file

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
 # Start cloudwatch agent
 
 echo "AWS SESSION TOKEN $AWS_SESSION_TOKEN"
+mkdir -p /root/.aws
+echo "[default]" > /root/.aws/credentials
+echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> /root/.aws/credentials
+echo "aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >> /root/.aws/credentials
+echo "aws_session_token = $AWS_SESSION_TOKEN" >> >> /root/.aws/credentials
+
 
 /opt/aws/amazon-cloudwatch-agent/bin/start-amazon-cloudwatch-agent&
 sleep 10
