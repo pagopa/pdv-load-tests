97227c80a4a70ab747870d115d87d2780c5c360e
uolter <walter.traspadini@pagopa.it>
2022-06-16 16:57:47 +0200

    start.sh script --> print cloudwatch agent log

--- start.sh
+++ start.sh
@@ -23,4 +23,4 @@ sleep 10
 K6_STATSD_ENABLE_TAGS=true k6 run --out statsd $1
 
 sleep 60
-# cat /opt/aws/amazon-cloudwatch-agent/logs/amazon-cloudwatch-agent.log
+cat /opt/aws/amazon-cloudwatch-agent/logs/amazon-cloudwatch-agent.log
