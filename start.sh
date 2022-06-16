09af46298ea31dbdb2acd04bfcd32f8397e53ca0
uolter <walter.traspadini@pagopa.it>
2022-06-16 12:34:46 +0200

    changed credentials profile name

--- start.sh
+++ start.sh
@@ -2,10 +2,11 @@
 
 echo "AWS SESSION TOKEN $AWS_SESSION_TOKEN"
 mkdir -p /root/.aws
-echo "[default]" > /root/.aws/credentials
+echo "[AmazonCloudWatchAgent]" > /root/.aws/credentials
 echo "aws_access_key_id = $AWS_ACCESS_KEY_ID" >> /root/.aws/credentials
 echo "aws_secret_access_key = $AWS_SECRET_ACCESS_KEY" >> /root/.aws/credentials
 echo "aws_session_token = $AWS_SESSION_TOKEN" >> /root/.aws/credentials
+echo "region = eu-south-1" >> /root/.aws/credentials
 
 
 /opt/aws/amazon-cloudwatch-agent/bin/start-amazon-cloudwatch-agent&
