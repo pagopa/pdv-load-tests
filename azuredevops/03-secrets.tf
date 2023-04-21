0a92bd8c928f862ba25a31215d5ce5e03e2d276e
uolter <walter.traspadini@pagopa.it>
2023-04-21 10:57:33 +0200

    uat confg and delete azure devops code

--- azuredevops/03-secrets.tf
+++
@@ -1,7 +0,0 @@
-data "aws_secretsmanager_secret" "devops_uat" {
-  name = "devops"
-}
-
-data "aws_secretsmanager_secret_version" "devops_uat" {
-  secret_id = data.aws_secretsmanager_secret.devops_uat.id
-}
